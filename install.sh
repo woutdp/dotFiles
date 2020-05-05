d078ab25d29f70b3b3bac4b90186d4e197eb5f97
Wout De Puysseleir <woutdp@gmail.com>
2020-05-04 18:35:57 -0700

    Various

---
+++ install.sh
@@ -0,0 +1,12 @@
+exec yay -S --needed \
+  base-devel git lazygit npm python python-pip docker docker-compose \
+  feh \
+  fish exa \
+  gotop \
+  kitty \
+  polybar \
+  vlc \
+  pulseaudio-ctl \
+  xfce4-power-manager \
+  wmutils-git \
+  neovim code pycharm-professional
